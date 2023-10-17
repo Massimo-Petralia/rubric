5abf4223ed79815b917f9536a893539ed8da7b40
massimo <massipetra@hotmail.it>
2023-10-17 17:49:31 +0200

    feat: create form group and collapse panel

--- src/app/features/rubric/rubric-new-contact/rubric-new-contact.component.ts
+++ src/app/features/rubric/rubric-new-contact/rubric-new-contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-rubric-new-contact',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./rubric-new-contact.component.scss']
 })
 export class RubricNewContactComponent {
+
+  constructor(private formBuilder: FormBuilder){}
+
+  form = this.formBuilder.group({
+    name: this.formBuilder.control<string>(''),
+    surname: this.formBuilder.control<string>(''),
+    email: this.formBuilder.control<string>(''),
+    phone: this.formBuilder.control<string>(''),
+  })
 
 }
