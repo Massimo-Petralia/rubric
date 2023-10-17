365656dec7a5e9aedf67595338c29aecda846b84
massimo <massipetra@hotmail.it>
2023-10-17 12:55:43 +0200

    feat: create form group property in RubricContactComponent

--- src/app/features/rubric/rubric-contact/rubric-contact.component.ts
+++ src/app/features/rubric/rubric-contact/rubric-contact.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-rubric-contact',
   templateUrl: './rubric-contact.component.html',
-  styleUrls: ['./rubric-contact.component.scss']
+  styleUrls: ['./rubric-contact.component.scss'],
 })
 export class RubricContactComponent {
+  constructor(private formBuilder: FormBuilder) {}
 
+  form = this.formBuilder.group({
+    name: this.formBuilder.control<string>(''),
+    surname: this.formBuilder.control<string>(''),
+    email: this.formBuilder.control<string>(''),
+    phone: this.formBuilder.control<string>(''),
+  });
 }
