4e28e8c5adadd6f933b2fe0a495318fdf202d6bd
massimo <massipetra@hotmail.it>
2023-10-17 11:41:28 +0200

    feat: import zorro modules and add contacts property on RubricContactsComponent

--- src/app/features/rubric/rubric-contacts/rubric-contacts.component.ts
+++ src/app/features/rubric/rubric-contacts/rubric-contacts.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Contact } from 'src/app/models/contact';
 
 @Component({
   selector: 'app-rubric-contacts',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./rubric-contacts.component.scss']
 })
 export class RubricContactsComponent {
+  
+  contacts : Contact[] =[]
 
 }
