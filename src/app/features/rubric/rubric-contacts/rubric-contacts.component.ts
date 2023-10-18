78ea2be4575ec0770065686684f51b008bf35c8a
massimo <massipetra@hotmail.it>
2023-10-18 16:01:45 +0200

    feat sort alphabetically

--- src/app/features/rubric/rubric-contacts/rubric-contacts.component.ts
+++ src/app/features/rubric/rubric-contacts/rubric-contacts.component.ts
@@ -4,26 +4,38 @@ import { Contact } from 'src/app/models/contact';
 @Component({
   selector: 'app-rubric-contacts',
   templateUrl: './rubric-contacts.component.html',
-  styleUrls: ['./rubric-contacts.component.scss']
+  styleUrls: ['./rubric-contacts.component.scss'],
 })
 export class RubricContactsComponent {
+  @Input() contacts: Contact[] = [];
 
-@Input()  contacts : Contact[] =[]
+  @Output() create = new EventEmitter<Contact>();
+  @Output() delete = new EventEmitter<number | null>();
+  @Output() save = new EventEmitter<Contact>();
 
-@Output() create = new EventEmitter<Contact>()
-@Output() delete = new EventEmitter<number | null>()
-@Output() save = new EventEmitter<Contact>()
+  onCreate(contact: Contact) {
+    this.create.emit(contact);
+  }
 
-onCreate(contact: Contact){
-  this.create.emit(contact)
-}
+  onDelete(id: number | null) {
+    this.delete.emit(id);
+  }
 
-onDelete(id: number | null ){
-  this.delete.emit(id)
-}
+  onSave(contact: Contact) {
+    this.save.emit(contact);
+  }
 
-onSave(contact: Contact){
-  this.save.emit(contact)
-}
-
+  sortAlphabetically() {
+    this.contacts = this.contacts.sort((a, b) => {
+      const charA = a.name!.charAt(0).toUpperCase();
+      const charB = b.name!.charAt(0).toUpperCase();
+      if (charA < charB) {
+        return -1;
+      }
+      if (charA > charB) {
+        return 1;
+      }
+      return 0;
+    });
+  }
 }
