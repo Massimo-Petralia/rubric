1c6882c1fe6f141752a35494d8e079bba68c9cba
massimo <massipetra@hotmail.it>
2023-10-18 11:10:34 +0200

    feat: add delete contact functionality

--- src/app/features/rubric/rubric-page/rubric-page.component.ts
+++ src/app/features/rubric/rubric-page/rubric-page.component.ts
@@ -6,34 +6,38 @@ import { DataService } from 'src/app/services/data.service';
 @Component({
   selector: 'app-rubric-page',
   templateUrl: './rubric-page.component.html',
-  styleUrls: ['./rubric-page.component.scss']
+  styleUrls: ['./rubric-page.component.scss'],
 })
 export class RubricPageComponent implements OnInit {
+  constructor(private dataService: DataService) {}
 
- constructor(private dataService: DataService){} 
+  contacts: Contact[] = [];
 
-contacts: Contact[] = []
+  subs = new Subscription();
 
-subs = new Subscription()
+  ngOnInit(): void {
+    this.subs.add(
+      this.dataService.getData().subscribe((contacts) => {
+        this.contacts = contacts;
+      })
+    );
+  }
 
-ngOnInit(): void {
-  this.subs.add(
-    this.dataService.getData().subscribe((contacts)=> {
-      this.contacts = contacts
-    })
-  )
-}
+  onCreate(contact: Contact) {
+    this.subs.add(
+      this.dataService
+        .createContact(contact)
+        .subscribe((contact) => (this.contacts = [...this.contacts, contact]))
+    );
+  }
 
-onCreate(contact: Contact){
-  this.subs.add(
-    this.dataService.createContact(contact).subscribe((contact)=> 
-     this.contacts = [...this.contacts, contact]
-    )
-  )
-}
-
-onDelete(id: number | null){
-  
-}
-
+  onDelete(id: number | null) {
+    this.subs.add(
+      this.dataService.deleteContact(id).subscribe(() => {
+        this.contacts = this.contacts.filter((contact) => 
+          contact.id !== id
+        );
+      })
+    );
+  }
 }
